f42916a476028a62a47eccb7a73661d74a80af4a
Qasim Afzal <qasim.afzal404@gmail.com>
2024-09-17 01:18:05 -0700

    yes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
+import 'package:chatapp/Services/navigation_services.dart';
+import 'package:chatapp/auth/auth_services.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,6 +11,10 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final GetIt getit = GetIt.instance;
+
+  late AuthServices _authServices;
+  late NavigationService _navigationService;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
